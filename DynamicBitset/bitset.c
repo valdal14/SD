ed6dd52a863b1ba98d475e22dc1617055f34ae03
Valerio DAlessio <u105550@canda.com>
2026-08-12 07:12:01 +0200

    feat(bitset): add the bitset header and imported the header into the implementation bitset.c to verify the link

---
+++ DynamicBitset/bitset.c
@@ -0,0 +1,11 @@
+#include "bitset.h"
+//#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    uint8_t x = 0;
+    x |= BIT(1);
+    printf("%d\n",x); 
+    return 0;
+}
---
+++ DynamicBitset/bitset.h
@@ -0,0 +1,55 @@
+#ifndef BITSET_H
+#define BITSET_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#define BIT(x) (1 << (x))
+
+typedef struct
+{
+    uint64_t *array;
+    size_t capacity;
+} bitset_t;
+
+/**
+ * @brief Creates an instance of the bitset_t struct
+ * @param size_t num_bits
+ * @return bitset_t
+ */
+bitset_t bitset_create(size_t num_bits);
+
+/**
+ * @brief Destroyes the bitset_t instance
+ * @param bitset_t set pointer
+ * @return void 
+ */
+void bitset_destroy(bitset_t *set);
+
+
+/**
+ * @brief Sets the bit at given index 
+ * @param bitset_t set pointer
+ * @param size_t index
+ * @return void
+ */
+void bitset_set(bitset_t *set, size_t index);
+
+/**
+ * @brief Sets the bit at index 0
+ * @param bitset_t set pointer
+ * @param size_t index
+ * @return void
+ */
+void bitset_clear(bitset_t *set, size_t index);
+
+/**
+ * @brief Returns true (1) if set, otherwise false (0)
+ * @param bitset_t set pointer
+ * @param size_t index
+ * @return void
+ */
+bool bitset_test(bitset_t *set, size_t index);
+
+#endif
