3ae03ed6f02fed201e77772334d29222d88b39ce
Valerio DAlessio <u105550@canda.com>
2026-08-14 07:54:59 +0200

    feat(circula_buffer): Final commit

--- CircularBuffer/pos.c
+++ CircularBuffer/pos.c
@@ -5,7 +5,7 @@
 #define QUEUE_SIZE 5
 #define DEF_TRANS_MASK 0x8
 #define INV_TRANS_MASK 0x0
-#define BIT(x) (1 << (x))
+#define BIT(x) (1U << (x))
 #define PRO_BIT 0
 #define DEL_BIT 1
 #define VAL_BIT 3
@@ -14,6 +14,7 @@
 typedef struct Transaction
 {
     uint64_t amount; // 8 bytes
+    uint32_t id; // 4 byte
     /**
      * bit 0 = processed
      * bit 2 = delivered
@@ -21,7 +22,7 @@ typedef struct Transaction
      * bit 8 = stored
      */
     uint8_t flag; // 1 byte
-    uint8_t _paddings[7]; // 7 bytes paddings
+    uint8_t _paddings[3]; // 3 bytes paddings
     // Total 16 bytes 
 } Transaction;
 
@@ -45,9 +46,11 @@ void init_queue(Queue **queue);
  * @brief Processes a transaction 
  * @param Queue queue pointer
  * @param Transaction transaction pointer
+ * @param void(*proc)(Queue *queue, uint8_t index) callback 
+ * to process and deliver the transaction
  * @return void
  */
-void process_transaction(Queue *queue, Transaction *transaction);
+void process_transaction(Queue *queue, Transaction *transaction, void(*proc)(Queue *queue, uint8_t index));
 
 /**
  * @brief Helper method to setup transaction examples
@@ -62,9 +65,27 @@ void setup_transactions(Transaction **transactions, size_t trans_amt);
  * @param Queue queue pointer
  * @param Transaction transactions pointer
  * @param size_t size 
+ * @param void(*proc)(Queue *queue, uint8_t index) callback 
+ * to process and deliver the transaction
  * @return void
  */
-void run(Queue *queue, Transaction *transactions, size_t size);
+void run(Queue *queue, Transaction *transactions, size_t size, void(*proc)(Queue *queue, uint8_t index));
+
+/**
+ * @brief Processes and Delivers the Transaction
+ * @param Queue queue pointer 
+ * @param uint8_t index 
+ * @return void
+ */
+void deliver(Queue *queue, uint8_t index);
+
+/**
+ * @brief Clean ups all allocated memory
+ * @param Queue queue pointer
+ * @param Transaction transactions pointer
+ * @return void
+ */
+void clean(Queue *queue, Transaction *transactions);
 
 int main(void)
 {
@@ -76,7 +97,9 @@ int main(void)
     printf("Queue up and running at address: %p\n", queue);
     printf("Sample Transactions created at address: %p\n", transactions);
     // Processing the Transactions
-    run(queue, transactions, trans_amt);
+    run(queue, transactions, trans_amt, deliver);
+    clean(queue, transactions);
+    
     return 0;
 }
 
@@ -117,37 +140,71 @@ void setup_transactions(Transaction **transactions, size_t trans_amt)
 
     for(uint8_t i = 0; i < trans_amt; i++)
     {
-        Transaction *t1 = (Transaction *)calloc(1, sizeof(Transaction));
-        if(t1 == NULL) exit(EXIT_FAILURE);
-        t1->amount = 14 + i;
+        Transaction t1 = {0};
+        t1.id = (i + 1);
+        t1.amount = 14 + i;
 
         if(i == 3) 
-            t1->flag = INV_TRANS_MASK;
+            t1.flag = INV_TRANS_MASK;
         else 
-            t1->flag = DEF_TRANS_MASK;
+            t1.flag = DEF_TRANS_MASK;
 
-        trans[i] = *t1;
+        trans[i] = t1;
     }
 
     *transactions = trans;
 }
 
-void process_transaction(Queue *queue, Transaction *transaction)
+void process_transaction(Queue *queue, Transaction *transaction, void(*proc)(Queue *queue, uint8_t index))
 {
-    if(queue->index == queue->capacity) queue->index = 0;
+    if((transaction->flag & BIT(VAL_BIT)) == 0)
+    {
+        printf("----------------------------------\n");
+        printf("Invalid Transaction: \n");
+        printf("STATUS = %d\n", transaction->flag); 
+        printf("AMOUNT = %llu\n", transaction->amount); 
+        printf("This Transaction will be discarded\n");
+        printf("----------------------------------\n");
+    }
+    else
+    {
+        // set the transaction flags
+        transaction->flag |= BIT(0);
 
-    queue->index = queue->index % queue->capacity;
-    printf("idx = %d\n", queue->index);
-    queue->transactions[queue->index] = *transaction;
-    printf("AMT = %llu\n", queue->transactions[queue->index].amount); 
-    printf("TRA = %d\n", queue->transactions[queue->index].flag); 
-    queue->index += 1;
-    printf("--------------------\n");
+        // Store the transaction at the current safe index
+        queue->transactions[queue->index] = *transaction;
+
+        // Process and deliver using the current safe index
+        proc(queue, queue->index);
+
+        // Increment the index 
+        queue->index = (queue->index + 1) % queue->capacity;;
+    }
 }
 
 
-void run(Queue *queue, Transaction *transactions, size_t size)
+void run(Queue *queue, Transaction *transactions, size_t size, void(*proc)(Queue *queue, uint8_t index))
 {
     for(uint8_t i = 0; i < size; i++)
-        process_transaction(queue, &transactions[i]);
+        process_transaction(queue, &transactions[i], proc);
+}
+
+
+void deliver(Queue *queue, uint8_t index)
+{
+    printf("--------------------------------------\n");
+    printf("Delivering Transaction ID  : %d\n", queue->transactions[index].id);
+    printf("Transaction Stored at index: %d\n", index);
+    printf("Transaction Amount         : %llu\n", queue->transactions[index].amount);
+    printf("--------------------------------------\n");
+    // set the flag to deliver 
+    queue->transactions[index].flag |= BIT(DEL_BIT);
+    queue->transactions[index].flag |= BIT(STO_BIT);
+}
+
+void clean(Queue *queue, Transaction *transactions)
+{
+    if(transactions != NULL) free(transactions);
+    if(queue->transactions != NULL) free(queue->transactions);
+    if(queue != NULL) free(queue);
 }
